4ee2ece07e1112ba67a29b5221f7f9fe168b5485
camillerose <camilletoarmino@gmail.com>
2019-05-03 09:59:18 -0400

    updated notebook

--- main_function_integration/get_plots.py
+++ main_function_integration/get_plots.py
@@ -1,19 +1,30 @@
 def sentiment(df,hashtag):
     
+    from matplotlib.pyplot import figure
+    import matplotlib.patches as mpatches
     import matplotlib.pyplot as plt
     import pandas as pd
-
-
+    
+    
+    # for duplicate index error
+    cols = ['datetime','sentiment']
+    df = df[cols]
     df['datetime'] = pd.to_datetime(df.datetime)
     df = df.set_index('datetime')
+
+    plt.figure(num=None, figsize=(10, 5), dpi=80, facecolor='w', edgecolor='k')
+    plt.rcParams.update({'font.size': 13})
+
     avg_sent_per_day = df.sentiment.resample('h').mean()
     count_sent_per_day = df.sentiment.resample('h').count()
-    avg_sent_per_day.name = "Mean"
-    count_sent_per_day.name = "Count"
     avg_sent_per_day.plot(color = 'blue', grid=False)
+    plt.xlabel('Hour');
     plt.title(hashtag)
-    plt.legend(loc='upper left')
-    plt.xlabel('Hourly Change')
     count_sent_per_day.plot(color='red',grid=False,secondary_y=True)
-    plt.legend(loc='upper right')
+    plt.xlabel('Hour');
+
+    red_patch = mpatches.Patch(color='red', label='Count')
+    blue_patch = mpatches.Patch(color='blue', label='Sentiment')
+
+    plt.legend(handles=[red_patch, blue_patch],loc=9,frameon=False);
     return plt
